30f4db2162d8d0f4a024aeca77dc77a36c2b976b
sDev <salehesenga6@gmail.com>
2025-02-12 15:15:20 +0300

    first commit

---
+++ src/main/java/tz/mil/bima/BimaApplication.java
@@ -0,0 +1,13 @@
+package tz.mil.bima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tz/mil/bima/BimaApplicationTests.java
@@ -0,0 +1,13 @@
+package tz.mil.bima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
